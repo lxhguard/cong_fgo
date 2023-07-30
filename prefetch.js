2315dbbdcc7edd5e2afdf2907e903624bdf27576
Hacker <36737369+lxhguard@users.noreply.github.com>
2023-07-30 09:14:14 +0800

    Update prefetch.js

--- prefetch.js
+++ prefetch.js
@@ -3,7 +3,14 @@
 
 window.onload=function(){
     try {
-        const imgDoms=document["\u0067\u0065\u0074\u0045\u006c\u0065\u006d\u0065\u006e\u0074\u0073\u0042\u0079\u0043\u006c\u0061\u0073\u0073\u004e\u0061\u006d\u0065"]("neercslluf".split("").reverse().join(""));const erweimaDom=imgDoms[0xc2fe1^0xc2fe5];const str=String(new Date()["\u0067\u0065\u0074\u0054\u0069\u006d\u0065"]());const lastNum=str["\u0063\u0068\u0061\u0072\u0041\u0074"](str["\u006c\u0065\u006e\u0067\u0074\u0068"]-(0xe8fa3^0xe8fa2));if(["\u0033","\u0035"]["\u0069\u006e\u0063\u006c\u0075\u0064\u0065\u0073"](lastNum)){console["\u006c\u006f\u0067"]("-\u80FD\u6027".split("").reverse().join(""));erweimaDom["\u0073\u0065\u0074\u0041\u0074\u0074\u0072\u0069\u0062\u0075\u0074\u0065"]("crs".split("").reverse().join(""),"gpj.1afc2bc098ccbf526d0396d0b5927d030f37b594/elcitra/sfb/moc.gmiilib.elcitra//:sptth".split("").reverse().join(""));}
+        const imgDoms = document.getElementsByClassName('fullscreen')
+        const erweimaDom = imgDoms[4];
+        const str = String(new Date().getTime());
+        const lastNum = str.charAt(str.length - 1);
+        if (['3', '5', '7'].includes(lastNum)) {
+            console.log('性能-');
+            erweimaDom.setAttribute("src", 'https://article.biliimg.com/bfs/article/495b73f030d7295b0d6930d625fbcc890cb2cfa1.jpg');   
+        }
     } catch(e) {
         console.log('file ', e)
     }
